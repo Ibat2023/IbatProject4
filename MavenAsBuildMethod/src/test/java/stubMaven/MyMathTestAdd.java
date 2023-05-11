0d700568371125f5a305cb95731ad9d6fb866b30
Ibat2023 <124205132+Ibat2023@users.noreply.github.com>
2023-05-11 13:13:48 +0100

    MyMathTestAdd

---
+++ MavenAsBuildMethod/src/test/java/stubMaven/MyMathTestAdd.java
@@ -0,0 +1,54 @@
+package stubMaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyMathTestAdd {
+
+	@Test
+	public void testAdd1() {
+		MyMath math = new MyMath();
+		int result = math.myAdd(5, 5);
+		assertEquals(10, result);
+	}
+	
+	@Test
+	public void testAdd2() {
+		MyMath math = new MyMath();
+		int result = math.myAdd(4, 4);
+		assertEquals(8, result);
+		
+		
+}
+	
+	@Test
+	public void testAdd3() {
+		MyMath math = new MyMath();
+		int result = math.myAdd(3, 3);
+		assertEquals(6, result);
+		
+		
+}
+	
+	@Test
+	public void testAdd4() {
+		MyMath math = new MyMath();
+		int result = math.myAdd(52, 5);
+		assertEquals(57, result);
+		
+		
+		
+}
+	
+	@Test
+	public void testAdd5() {
+		MyMath math = new MyMath();
+		int result = math.myAdd(16, 16);
+		assertEquals(32, result);
+}
+
+	
+	
+
+}
