e35a918f5ddb6fbf1ea8a43bc45d90f5c047da6d
Ibat2023 <124205132+Ibat2023@users.noreply.github.com>
2023-05-11 11:22:12 +0100

    Stub methods in Maven

---
+++ MavenAsBuildMethod/src/main/java/stubMaven/MyMath.java
@@ -0,0 +1,20 @@
+package stubMaven;
+
+public class MyMath {
+	public int myAdd(int a, int b) {
+		return a+b;
+	}
+
+	public int mySubtract(int a, int b) {
+		return a-b;
+	}
+	
+	public int myMultiply(int a, int b) {
+		return a*b;
+		
+	}
+	
+	public int myDivide(int a, int b) {
+		return a/b;
+	}
+}
