4fc1715c111da6b574346374954a2923210d213d
Ibat2023 <124205132+Ibat2023@users.noreply.github.com>
2023-05-11 11:28:05 +0100

    JUnitTesting

---
+++ MavenAsBuildMethod/src/test/java/stubMaven/JUnitTestingAdd.java
@@ -0,0 +1,14 @@
+package stubMaven;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JUnitTestingAdd {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
