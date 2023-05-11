3aff9522712daec8f1b17abe88c81d7fc8416e51
Ibat2023 <124205132+Ibat2023@users.noreply.github.com>
2023-05-11 15:34:19 +0100

    MyMathTestDivide Test Cases

--- MavenAsBuildMethod/src/test/java/stubMaven/MyMathTestDivide.java
+++ MavenAsBuildMethod/src/test/java/stubMaven/MyMathTestDivide.java
@@ -7,11 +7,40 @@ import org.junit.Test;
 public class MyMathTestDivide {
 
 	@Test
-	public void testDivide() {
+	public void testDivide1() {
 		MyMath math = new MyMath();
 		int result = math.myDivide(10, 5);
 		assertEquals(2, result);
 		
 	}
+	
+	public void testDivide2() {
+		MyMath math = new MyMath();
+		int result = math.myDivide(20, 2);
+		assertEquals(10, result);
+		
+	}
+
+	public void testDivide3() {
+		MyMath math = new MyMath();
+		int result = math.myDivide(100, 5);
+		assertEquals(20, result);
+		
+	}
+
+	public void testDivide4() {
+		MyMath math = new MyMath();
+		int result = math.myDivide(50, 10);
+		assertEquals(5, result);
+		
+	}
+
+	public void testDivide5() {
+		MyMath math = new MyMath();
+		int result = math.myDivide(14, 2);
+		assertEquals(7, result);
+		
+	}
+
 
 }
