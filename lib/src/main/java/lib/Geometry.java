ff0681da4a10d257868b8b10ec4f5824ee421ecb
Ibat2023 <124205132+Ibat2023@users.noreply.github.com>
2023-06-09 16:04:56 +0100

    Update Geometry.java

--- lib/src/main/java/lib/Geometry.java
+++ lib/src/main/java/lib/Geometry.java
@@ -5,6 +5,23 @@ public class Geometry {
 	public float areaRect(float width, float height) {
 		float area = width*height;
 		return area;
+		
+	}
+	//Calculating the Perimetar of the rectangle
+	public static float perimetarRect(float width, float height) {
+		float perimetar = 2*(width+height);
+		return perimetar;
+		
+	}
+	
+	//Calculating the sphare volume
+	public float volumeSphere(float radius) {
+		float volume = (4.0f / 3.0f) * (float) Math.PI * radius * radius * radius;
+		return volume;
 	}
 
+	public float surfaceAreaSphere(float radius) {
+		float surface = 4.0f * (float) Math.PI * radius * radius;
+		return surface;
+	}
 }
